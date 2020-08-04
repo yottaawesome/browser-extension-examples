0edad3fdebff39ae40f1e4131353bd1ac5d6eb0e
Vasilios Magriplis <etameson@gmail.com>
2020-08-04 12:50:21 +1000

    Commit initial native messaging sample

---
+++ src/chrome/native-messaging/host/install-host.bat
@@ -0,0 +1,3 @@
+:: Change HKLM to HKCU if you want to install to the current user.
+:: %~dp0 is the directory containing this bat script and ends with a backslash.
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.billy.extension.nativemessaging" /ve /t REG_SZ /d "%~dp0nmh.json" /f
---
+++ src/chrome/native-messaging/host/uninstall-host.bat
@@ -0,0 +1,2 @@
+:: Deletes the entry created by install_host.bat
+REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.billy.extension.nativemessaging" /f
